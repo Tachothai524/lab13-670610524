8bfdabaa174cbab42b8aad41bfb7d35b0ec74e9f
tachothai524 <tachothai_th@cmu.ac.th>
2026-09-07 20:43:06 +0700

    lab13 finish

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -1,29 +1,63 @@
 import TaskCard from "../components/TaskCard";
 import TodoModal from "../components/Modal";
 import { type TaskCardProps } from "../libs/Todolist";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+
+const STORAGE_KEY = "lab13.tasks";
+
+function loadTasks(): TaskCardProps[] {
+  try {
+    const raw = localStorage.getItem(STORAGE_KEY);
+    return raw ? JSON.parse(raw) : [];
+  } catch {
+    return [];
+  }
+}
 
 function App() {
-  const [tasks, setTasks] = useState<TaskCardProps[]>([]);
+  const [tasks, setTasks] = useState<TaskCardProps[]>(loadTasks);
 
   const handleAdd = (newTask: TaskCardProps) => {
-    console.log("TODO handleAdd", newTask);
+    // console.log("TODO handleAdd", newTask);
+    setTasks([...tasks,newTask]);
   };
 
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+  }, [tasks]);
+
   const deleteTask = (taskId: string) => {
-    console.log("TODO deleteTask", taskId);
+    // console.log("TODO deleteTask", taskId);
+    setTasks(tasks.filter((t) => t.id !== taskId));
   };
 
   const toggleDoneTask = (taskId: string) => {
-    console.log("TODO toggleDoneTask", taskId);
+    // console.log("TODO toggleDoneTask", taskId);
+    setTasks(
+      tasks.map((t) => (t.id === taskId ? { ...t, isDone: !t.isDone } : t)),
+    );
   };
 
   return (
     <div className="col-12 m-2 p-0">
       <div className="container text-center">
         <h2>Todo List</h2>
-        <span className="m-2">All : () Done : ()</span>
-
+        <div className="container px-4 text-center">
+            <div className="row gap-4">
+            <div className="col">
+              <div className="fs-5">All : [
+                <span className="fs-5 text-primary">{tasks.length}</span>
+              ]
+              </div>
+            </div>
+            <div className="col">
+              <div className="fs-5">Done : [
+                <span className="fs-5 text-primary">{tasks.filter((t) => t.isDone === true).length}</span> 
+              ]
+              </div>
+            </div>
+          </div>
+        </div>
         <div>
           <button
             type="button"
